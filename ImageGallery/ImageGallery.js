61f62bb4ed91ee90017bdf030eeb2b0d389bc55b
Laure Guery <laureguery@hotmail.com>
2016-05-16 16:28:33 +0100

    add

--- ImageGallery/ImageGallery.js
+++ ImageGallery/ImageGallery.js
@@ -1,14 +1,14 @@
 var modal= document.getElementById('MyModal');
-var img=document.getElementById('MyImg');
+var img=document.getElementById("MyImg");
 var modalImg=document.getElementById("img_01");
-var captionText=document.getElementById("caption");
+//var captionText=document.getElementById("caption");
 
 img.onclick= function()
 {
     modal.style.display = "block";
     modalImg.src=this.src;
     modalImg.alt=this.alt;
-    captionText.innerHTML=this.alt;
+    //captionText.innerHTML=this.alt;
 }
 
 var span = document.getElementsByClassName("close")[0];
@@ -16,6 +16,8 @@ span.onclick = function ()
 {
     modal.style.display = "none";
 }
+
+// Create 2 hover function for 
 function hover(element) {
     element.setAttribute('src', "Picture/paris.jpg");
 }
